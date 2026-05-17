f37cfdaab57e2fd2f6f7dd1afd3908cff756ac0e
maxmoka <ma1x1moka@gmail.com>
2026-05-18 01:36:22 +0200

    feat: add CortexAccount with validateUserOp, execute and constructor

--- src/CortexAccount.sol
+++ src/CortexAccount.sol
@@ -1,11 +1,31 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.27;
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract CortexAccount {
+    constructor(address _enrtyPoint) {
+        entryPoint = IEntryPoint(_enrtyPoint);
+        owner = msg.sender;
+    }
+
     // erros
 
     error AccessDenied(address sender, uint256 time);
+    error callFailed(uint256 time);
+
+    // modifiers
+
+    modifier chackRights() {
+        if (msg.sender != address(entryPoint) && msg.sender != owner) {
+            revert AccessDenied(msg.sender, block.timestamp);
+        }
+        _;
+    }
+
     // structs
     struct UserOperation {
         address sender;
@@ -22,19 +42,30 @@ contract CortexAccount {
     }
     // varibles
     address public owner;
-    address public IEntryPoint  immutable entryPoint;
+    IEntryPoint public immutable entryPoint;
 
-    // modifiers
-    modifier Checkrights() {
+    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+
         if (msg.sender != address(entryPoint)) {
-            revert AccessDenied(msg.sender, block.timestamp);
+            return 1;
         }
-        _;
+
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+
+        if (signer != owner) {
+            return 1;
+        }
+        return 0;
     }
 
-    function validateUserOp(
-        UserOperation calldata userOp,
-        bytes32 userOpHash,
-        uint256 missingAccountFunds
-    ) external Checkrights returns (uint256 validationData) {}
+    function execute(address to, uint256 value, bytes calldata data) external chackRights {
+        (bool call,) = payable(to).call{value: value}(data);
+        if (call != true) {
+            revert callFailed(block.timestamp);
+        }
+    }
 }
